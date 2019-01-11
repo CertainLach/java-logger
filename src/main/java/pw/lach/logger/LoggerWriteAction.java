a65095c7f7e0b61332b0566abd991cd95af7b112
xDark <aleshkailyashevich@gmail.com>
2019-01-11 16:38:56 +0300

    Speed  up logger by using buffers and weak cache?

--- src/main/java/pw/lach/logger/LoggerWriteAction.java
+++ src/main/java/pw/lach/logger/LoggerWriteAction.java
@@ -4,17 +4,17 @@ import lombok.Data;
 
 @Data
 public class LoggerWriteAction {
-	LoggerAction type;
-	String name;
-	long time;
-	Object line;
-	Object[] params;
-	int progress;
+    LoggerAction type;
+    String name;
+    long time;
+    Object line;
+    Object[] params;
+    int progress;
 
-	long writtenAt;
-	String writtenFrom;
-	int identationLength;
+    long writtenAt;
+    String writtenFrom;
+    int identationLength;
 
-	int repeats=0;
-	boolean repeated;
+    int repeats = 0;
+    boolean repeated;
 }
